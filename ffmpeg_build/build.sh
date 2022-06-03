36e219bdb0276483c476c650fcc5554a4d86a354
raoqiang <973017553@qq.com>
2022-06-03 18:41:43 +0800

    1 增加SDL2源码安装脚本 2 build.sh脚本增加编译前支持包注释

--- ffmpeg_build/build.sh
+++ ffmpeg_build/build.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 
+#########################################################################
+# sudo apt-get update // 从服务器获取软件列表，并在本地保存为文件
+# sudo apt-get upgrade // 本地安装软件与本地软件列表对比，如本地安装版本低，会提示更新
+#
+# sudo apt-get install git // 下载源码
+# sudo apt-get install vim // 编辑器
+# sudo apt-get install make // 编译工具
+# sudo apt-get install cmake // x265所需要的编译工具
+#
+# 解决ffplay播放视频报错:Could not initialize SDL - No available video device问题
+# sudo apt-get install libx11-dev
+# sudo apt-get install xorg-dev
+#
+# 解决ffplay播放音频报错:Could not initialize SDL - Audio target 'pulse' not available,以及No such audio device问题
+# sudo apt-get install libpulse-dev
+# 以下这两个我只安装了libasound2也是可以的，如果不行就两个都安装
+# sudo apt-get install libasound2
+# sudo apt-get install libasound2-dev
+#########################################################################
+
 set -e
 
 source ./tools.sh
@@ -20,12 +40,12 @@ mkdir output/bin
 . build_libfdk_aac.sh
 . build_libmp3lame.sh
 . build_libopus.sh
+. build_sdl2.sh
 . build_yasm.sh
 . build_libvpx.sh
 . build_libx264.sh
 . build_libx265.sh
 . build_ffmpeg.sh
-
 
 endTime=`date +%Y%m%d-%H:%M:%S`
 endTime_s=`date +%s`
---
+++ ffmpeg_build/build_sdl2.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+# SDL2 生成ffplay需要
+set -e
+
+SDL2="SDL2"
+SDL2_VERSION="2.0.22"
+SDL2_URL="http://www.libsdl.org/release/"
+SDL2_CONFIGURE_COMMAND="./configure
+--prefix=$PREFIX
+--bindir=$PREFIX/bin
+"
+
+if [[ "$enableShared" == true  ]]; then
+ SDL2_CONFIGURE_COMMAND=$SDL2_CONFIGURE_COMMAND"
+ --enable-shared
+ --disable-static
+ "
+else
+ SDL2_CONFIGURE_COMMAND=$SDL2_CONFIGURE_COMMAND"
+ --enable-static
+ --disable-shared
+ "
+fi
+
+echo "==========================download SDL2=========================="
+if [ ! -e $SDL2".tar.gz" ]; then
+ if [[ $SYSTEM == "Darwin" ]]; then
+  curl $SDL2_URL$SDL2"-"$SDL2_VERSION".tar.gz" > $SDL2".tar.gz"
+ else
+  wget $SDL2_URL$SDL2"-"$SDL2_VERSION".tar.gz" -O $SDL2".tar.gz"
+ fi
+fi
+ 
+echo "==========================unzip SDL2=========================="
+if [ -e $SDL2".tar.gz" ]; then
+ if [ -e $SDL2"-"$SDL2_VERSION ]; then
+  rm -rf $SDL2"-"$SDL2_VERSION
+ fi
+ tar zxvf $SDL2".tar.gz"
+fi
+
+echo "==========================build SDL2=========================="
+if [ -e $SDL2"-"$SDL2_VERSION ]; then
+ cd $SDL2"-"$SDL2_VERSION
+ $SDL2_CONFIGURE_COMMAND
+ make clean
+ make -j${cpu_num}
+ make install
+fi
+cd $MY_DIR
+echo "==========================SDL2 build successful!=========================="
